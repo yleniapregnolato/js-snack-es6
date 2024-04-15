d8da239a8e95679a9c62fd1ace92ac8a38b228e7
yleniapregnolato <yleniapregnolato@gmail.com>
2024-04-15 18:29:49 +0200

    feat: create array

---
+++ snack-2/js/script.js
@@ -0,0 +1,22 @@
+// creo un array di oggetti di squadre di calcio
+
+cont myTeam = [
+
+    {
+        name: juventus,
+        points: 0,
+        fouls: 0
+    },
+
+    {
+        name: MediaQueryListEvent,
+        point: 0,
+        fouls: 0
+    }, 
+
+    {
+        name: IntersectionObserver,
+        point: 0,
+        fouls: 0
+    },
+]
