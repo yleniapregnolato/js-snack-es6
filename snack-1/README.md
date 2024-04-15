d1ca271e473e5274dffd7f02b18b91552212cffd
yleniapregnolato <yleniapregnolato@gmail.com>
2024-04-15 18:22:15 +0200

    feat: snack 1

--- snack-1/README.md
+++ snack-1/README.md
@@ -1,4 +1,4 @@
 # SNACK 1
 
-- [ ] creare un array di oggetti, ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+- [x] creare un array di oggetti, ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 - [ ] stampare a schermo la bici con peso minore utilizzando destructuring e template literal
---
+++ snack-1/js/script.js
@@ -0,0 +1,41 @@
+// creo un array di ogetti per descrivere le bici da corsa
+
+const myBikes = [
+    {
+        name: "bike-1",
+        weigth: 7.2
+    },
+
+    {
+        name: "bike-2",
+        weigth: 6.8
+    },
+
+    {
+        name: "bike-3",
+        weigth: 6.5
+    },
+]
+
+// definisco una variabile d'appoggio per salvare il risultato
+let ligthBike = "";
+
+// imposto un ciclo for per trovare la bici con il peso minore
+
+for (let i = 1; i < myBikes.length; i++) {
+    if (myBikes[i].weigth < 6.8) {
+        ligthBike = myBikes[i];
+    }
+}
+
+// prelevo l'elemento dal DOM tramite classe
+let bike = document.querySelector(".bike");
+
+// salvo i valori che desidero nella variabile tramite destructuring
+let {name, weigth} = ligthBike;
+
+// stampo in console il risultato
+console.log("la bici più leggera è ${name}");
+
+// stampo nel dom il risultato
+bike.innerHTML = `<div>la bici più leggera è ${name}</div>`;
