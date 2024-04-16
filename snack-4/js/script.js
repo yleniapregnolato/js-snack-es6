53843dfb77c1be76c853e80bfc1656cc12f42430
yleniapregnolato <yleniapregnolato@gmail.com>
2024-04-16 12:24:42 +0200

    feat: people array

---
+++ snack-4/js/script.js
@@ -0,0 +1,20 @@
+
+const people = [
+    {
+        nome: "mario",
+        cognome: "rossi",
+        età: 15
+    },
+
+    {
+        nome: "giovanni",
+        cognome: "bianchi",
+        età: 19
+    },
+
+    {
+        nome: "marco",
+        cognome: "ferrari",
+        età: 48
+    },
+]
