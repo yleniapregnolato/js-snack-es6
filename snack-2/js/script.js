a86eae15245044c0fb735ab2f9e2f561b072f0d9
yleniapregnolato <yleniapregnolato@gmail.com>
2024-04-16 10:46:05 +0200

    feat: generate random numbers

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,22 +1,34 @@
 // creo un array di oggetti di squadre di calcio
 
-cont myTeam = [
+cont myTeams = [
 
     {
-        name: juventus,
+        name: "juventus",
         points: 0,
         fouls: 0
     },
 
     {
-        name: MediaQueryListEvent,
+        name: "MediaQueryListEvent",
         point: 0,
         fouls: 0
     }, 
 
     {
-        name: IntersectionObserver,
+        name: "IntersectionObserver",
         point: 0,
         fouls: 0
     },
-]
+];
+
+// generare numeri random al posto degli 0 nelle proprietà points e fouls
+// possiamo utilizzare un foreach
+
+myTeams.forEach((curTeam) => {
+    curTeam.point = getRdnInteger(0, 50);
+    curTeam.fouls = getRdnInteger(0, 50);  
+});
+
+console.log(myTeams);
+
+
