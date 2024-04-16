3e6b34b6884c4c12e1345bd911156a391952dd36
yleniapregnolato <yleniapregnolato@gmail.com>
2024-04-16 11:37:27 +0200

    add: new progect scaffolding

---
+++ snack-3/README.md
@@ -0,0 +1,8 @@
+- Crea un array composto da 4 automobili.
+ - Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+- Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+
+## PROCEDIMENTO
+- imposto un ciclo foreach per prelevare ogni categoria di auto
+- salvo il risultato in una variabile di appoggio
+- con filter creo le nuove array che soddisfano le varie condizioni
---
+++ snack-3/js/script.js
@@ -0,0 +1,14 @@
+//  creare un array composta da 4 automobili
+const automobili = [
+    { marca: "Toyota", modello: "Yaris", alimentazione: "ibrida" },
+    { marca: "Volkswagen", modello: "Golf", alimentazione: "benzina" },
+    { marca: "BMW", modello: "i3", alimentazione: "elettrico" },
+    { marca: "Fiat", modello: "Panda", alimentazione: "metano" },
+    { marca: "Renault", modello: "Clio", alimentazione: "diesel" },
+    { marca: "Tesla", modello: "Model S", alimentazione: "elettrico" },
+    { marca: "Ford", modello: "Focus", alimentazione: "benzina" },
+    { marca: "Audi", modello: "A3", alimentazione: "diesel" },
+    { marca: "Mercedes-Benz", modello: "EQC", alimentazione: "elettrico" },
+    { marca: "Opel", modello: "Corsa", alimentazione: "gpl" }
+  ];
+
